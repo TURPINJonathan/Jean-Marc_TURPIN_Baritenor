e4a32f55557c767efe4a2de8661bb6139c213f76
TURPIN Jonathan <turpin.j@hotmail.fr>
2024-01-03 15:28:56 +0100

    [INSTALL] - Alias resolve configurations (tsconfig.app.json + vite.config.ts folders)

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -10,7 +10,14 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+      '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
     }
   }
 })
