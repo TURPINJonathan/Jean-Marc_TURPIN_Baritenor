6155c33389867b29c1247545211f9976dc950186
TURPIN Jonathan <turpin.j@hotmail.fr>
2024-01-13 12:47:30 +0100

    Top header text with effects done

---
+++ front/src/components/layout/HeaderLayout.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const title = ref('Jean-Marc TURPIN')
+const visibleTitle = ref('')
+const isDefinitionShow = ref(false)
+
+const showLetter = (index: number) => {
+  visibleTitle.value += title.value.charAt(index)
+}
+
+const revealTitle = () => {
+  let index = 0
+  const interval = [200, 300, 250, 350, 280] // intervalles irréguliers
+
+  const showNextLetter = () => {
+    if (index < title.value.length) {
+      showLetter(index)
+      index++
+      setTimeout(showNextLetter, interval[Math.floor(Math.random() * interval.length)])
+    }
+  }
+
+  showNextLetter()
+}
+
+onMounted(() => {
+  revealTitle()
+})
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1>{{ visibleTitle }}</h1>
+      <p @mouseover="isDefinitionShow = true" @mouseleave="isDefinitionShow = false">Bariténor</p>
+      <div>
+        <small v-show="isDefinitionShow">
+          Voix masculine intermédiaire entre le ténor et la basse.
+        </small>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  border: 1px solid red;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  div {
+    h1 {
+      text-align: center;
+    }
+
+    p {
+      text-align: center;
+      letter-spacing: 0.6rem;
+      font-size: 2rem;
+      text-transform: uppercase;
+    }
+
+    div {
+      text-align: center;
+      font-style: italic;
+      margin-top: 1rem;
+      font-size: 1.2rem;
+      height: 1.5rem;
+    }
+  }
+}
+</style>
+
+<script lang="ts">
+export default {
+  name: 'HeaderLayout'
+}
+</script>
