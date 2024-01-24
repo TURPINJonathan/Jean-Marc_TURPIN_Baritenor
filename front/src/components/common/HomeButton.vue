e5dd6cba7d8cb91594254a4118dae57e5463dcc6
TURPIN Jonathan <turpin.j@hotmail.fr>
2024-01-24 22:30:20 +0100

    [FRONT] Add contact page layout

---
+++ front/src/components/common/HomeButton.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <section>
+    <RouterLink to="/">Accueil</RouterLink>
+  </section>
+</template>
+
+<style scoped lang="scss">
+section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 1rem;
+
+  & > * {
+    width: 250px;
+    height: 50px;
+    color: rgb(241, 241, 241);
+    border: 1px solid rgb(241, 241, 241);
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+}
+</style>
